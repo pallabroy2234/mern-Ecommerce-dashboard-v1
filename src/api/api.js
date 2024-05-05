fc2bdd333bcd0a15e61c35352efa2870b5c7faa1
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-06 00:09:11 +0600

    config development and production mode

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 
+// Development URL
+const developmentUrl = "http://localhost:3000";
+
+// Production URL
+
+const productionUrl = "https://mern-ecommerce-backend-v1-w1ps.onrender.com"; // Replace this with your actual production URL
+
+// Determine the mode (development or production)
+const mode = "production"; // Change this to "production" when deploying
+
+// Select the appropriate API URL based on the mode
+const apiBaseUrl = mode === "production" ? productionUrl : developmentUrl;
+
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    // baseURL: "http://localhost:3000/api"
+    baseURL: `${apiBaseUrl}/api/`,
+    withCredentials: true,
 })
 
 export default api
-
-//
