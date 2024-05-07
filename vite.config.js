4720664954633713c4cc16f0d39965c2d9748fe9
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-07 12:45:41 +0600

    vite server config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4174, // Set the desired port number
+    host: "0.0.0.0", // Bind to all network interfaces
+  },
 })
