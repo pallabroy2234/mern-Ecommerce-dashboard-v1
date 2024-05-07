20d52a58474342aaef7f10f58bed23ab292797f5
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-07 15:23:45 +0600

    vite server port config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-      proxy: {
-        "/api" : {
-          target: "https://mern-ecommerce-backend-v1-w1ps.onrender.com",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, "")
-        }
-      }
-  }
+    plugins: [react()],
+    server: {
+        port: 5173,
+        cors: true,
+        host: "0.0.0.0", // Bind to all network interfaces
+    }
 })
